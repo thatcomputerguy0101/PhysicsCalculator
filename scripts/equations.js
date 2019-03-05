b0d0f4dcf5b4b0d05ba8ae6c92185005b7ef280e
thatcomputerguy <thatcomputerguy0101@gmail.com>
2019-03-05 16:06:59 -0500

    Escape . in regex

--- scripts/equations.js
+++ scripts/equations.js
@@ -339,7 +339,7 @@ var data = { // eslint-disable-line no-unused-vars
                 eq.current = operate(eq.current.replace(/\\left\[|\\right\]/g, " "), "eval");
                 eq.answer = eq.current.split(/ ?= ?/);
                 eq.answer[1] = eq.answer[1].replace("\\right]", "").split("\\left[");
-                eq.history.forEach((tex, i) => eq.history[i] = tex.replace(/\d+.?\d*/g, (num) => math.format(parseFloat(num), 3)));
+                eq.history.forEach((tex, i) => eq.history[i] = tex.replace(/\d+\.?\d*/g, (num) => math.format(parseFloat(num), 3)));
             }
         },
         {
